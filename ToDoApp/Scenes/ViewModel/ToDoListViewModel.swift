0fafb255b7e17bd11872d19e6c193f62586b8730
Ryniere Silva <ryniere@espresslabs.com>
2020-09-05 17:01:24 -0400

    installing RxSwift

---
+++ ToDoApp/Scenes/ViewModel/ToDoListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListViewModel.swift
+//  ToDoApp
+//
+//  Created by Ryniere dos Santos Silva on 2020-09-05.
+//  Copyright © 2020 Ryniere dos Santos Silva. All rights reserved.
+//
+
+import Foundation
+
+
+
+class ToDoListViewModel {
+    
+}
