6995db803f35ee41b6628dd134d3af5019747245
Ryniere Silva <ryniere@espresslabs.com>
2020-09-06 18:26:06 -0400

    Adding unit test

--- ToDoAppTests/ToDoAppTests.swift
+++ ToDoAppTests/ToDoAppTests.swift
@@ -19,9 +19,21 @@ class ToDoAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFetchTasks() throws {
+        
+        let taskService = TaskService.shared
+        
+        let expectation = XCTestExpectation(description: "Fetching tasks")
+        
+        taskService.fetchTasks(successHandler: { (tasks) in
+            XCTAssertNotNil(tasks)
+            expectation.fulfill()
+        }) { (error) in
+            
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+        
     }
 
     func testPerformanceExample() throws {
